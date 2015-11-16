78ac3f0a9b3c9af17da6ea33ef3b728246e2d9c3
trongbang <trongbang86@gmail.com>
2015-11-16 15:26:52 +1100

    added an unit test for HomeController and added ng-html2js to handle template issues

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,31 @@
 // Karma configuration
 // Generated on Sat Nov 14 2015 22:37:58 GMT+1100 (AEDT)
-var vendorJsFiles = require('./vendor.json');
+
 
 var _ = require('underscore');
 
-var appJsFiles = [
-  'app/scripts/**/*.js',
-  'test/unit/**/*.js'
-];
 
-var jsFiles = _.union(vendorJsFiles,appJsFiles);
+/**
+ * This function returns a list of js/html files 
+ * to be loaded to karma runner
+ */
+function filesArray(){
+
+    var vendorJsFiles = require('./vendor.json');
+
+    var mockJsFiles = [
+        'bower_components/angular-mocks/angular-mocks.js'
+    ];
+
+    var appJsFiles = [
+      'app/scripts/**/*.js',
+      'test/unit/**/*.js'
+    ];
+
+    var htmlFiles = ['app/templates/**/*.html'];
+
+    return _.union(vendorJsFiles, mockJsFiles, appJsFiles, htmlFiles);
+}
 
 module.exports = function(config) {
   config.set({
@@ -17,14 +33,13 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'sinon'],
 
 
     // list of files / patterns to load in the browser
-    files: jsFiles,
+    files: filesArray(),
 
 
     // list of files to exclude
@@ -34,9 +49,17 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'app/scripts/**/*.js': ['coverage']
+        'app/scripts/**/*.js': ['coverage'],
+        'app/templates/**/*.html': ['ng-html2js']
     },
 
+    // settings for ng-html2js preprocessor
+    ngHtml2JsPreprocessor: {
+        stripPrefix: 'app/',
+        moduleName: 'AppTemplate'
+    },
+
+    // settings for coverage plugin
     coverageReporter: {
         type: 'html',
         dir: 'coverage/'
@@ -69,6 +92,11 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome', 'Firefox'],
 
+    client: {
+        mocha: {
+            timeout: '5000'
+        }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
--- test/unit/test.js
+++
@@ -1,5 +0,0 @@
-describe('Test', function(){
-	it('will fails', function(){
-		expect(false).not.to.eq(true);
-	})
-});
